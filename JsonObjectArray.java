691f78f0ec6691c9611b6e2c5accfae088e1a53f
Devin Kirby <devinckirby@gmail.com>
2015-07-29 16:09:37 -0400

    Add URLJSONString and JsonObjectArray classes

---
+++ JsonObjectArray.java
@@ -0,0 +1,23 @@
+package com.kirby.wiki;
+import java.io.IOException;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+
+public class JsonObjectArray {
+	public static void main(String[] args) throws IOException, JSONException{
+		JSONObject jsonObj = new JSONObject(URLJSONString.parseJSON());
+		JSONArray jsonMainArr = jsonObj.getJSONArray("results");
+		for (int i = 0; i < jsonMainArr.length(); i++) {  // **line 2**
+		     JSONObject childJSONObject = jsonMainArr.getJSONObject(i);
+		     String name = childJSONObject.getString("name");
+		     String address = childJSONObject.getString("formatted_address");
+		     System.out.println(name + "" + address);
+		     
+		}
+	
+	}
+
+}
---
+++ URLJSONString.java
@@ -0,0 +1,29 @@
+package com.kirby.wiki;
+import java.io.*;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
+import org.json.JSONException;
+
+public class URLJSONString {
+	public static String parseJSON() throws IOException, JSONException{
+		StringBuilder jsonResult = null;
+		try {
+			URL google = new URL(
+					"https://maps.googleapis.com/maps/api/place/textsearch/json?query=pizza+in+Detroit&key=AIzaSyAxC5yolZSd5G-UiQc70px_8cX0T09mqNs");
+			URLConnection yc = google.openConnection();
+			BufferedReader in = new BufferedReader(new InputStreamReader(
+					yc.getInputStream()));
+			String inputLine;
+			jsonResult = new StringBuilder();
+			while ((inputLine = in.readLine()) != null) {
+				jsonResult.append(inputLine);
+			}
+		} catch (MalformedURLException e) {
+			System.out.println("MalformedURL");
+		}
+		return jsonResult.toString();
+	}
+}
+
